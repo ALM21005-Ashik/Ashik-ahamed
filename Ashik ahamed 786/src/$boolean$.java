025d2b303d336970c50b4f1d92c1def4a2551089
Lenova <Lenova@DESKTOP-NSGHHQJ>
2021-07-12 22:43:44 -0700

    first commit

---
+++ Ashik ahamed 786/src/$boolean$.java
@@ -0,0 +1,22 @@
+
+public class $boolean$ {
+
+	public static void main(String[] args) {
+		int i=10;
+		int j=20;
+		
+		boolean result=i==j;
+		System.out.println(result);
+		
+		
+		System.out.println(i<j);
+		System.out.println(i>j);
+		System.out.println(i<=j);
+		System.out.println(i>=j);
+		System.out.println(i!=j);
+		
+		
+
+	}
+
+}
